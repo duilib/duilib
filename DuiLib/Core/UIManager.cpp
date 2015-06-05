d2dae550023d142c25a84339a79750865f458fe7
daviyang35 <daviyang35@gmail.com>
2015-06-05 16:15:00 +0800

    修复在Button中弹出模态窗口时，可能导致的heap错误

--- DuiLib/Core/UIManager.cpp
+++ DuiLib/Core/UIManager.cpp
@@ -955,8 +955,16 @@ bool CPaintManagerUI::MessageHandler(UINT uMsg, WPARAM wParam, LPARAM lParam, LR
             event.ptMouse = pt;
             event.wKeyState = (WORD)wParam;
             event.dwTimestamp = ::GetTickCount();
+			// By daviyang35 at 2015-6-5 16:10:13
+			// 在Click事件中弹出了模态对话框，退出阶段窗口实例可能已经删除
+			// this成员属性赋值将会导致heap错误
+			// this成员函数调用将会导致野指针异常
+			// 使用栈上的成员来调用响应，提前清空成员
+			// 当阻塞的模态窗口返回时，回栈阶段不访问任何类实例方法或属性
+			// 将不会触发异常
+			CControlUI* pClick = m_pEventClick;
+			m_pEventClick = NULL;
             m_pEventClick->Event(event);
-            m_pEventClick = NULL;
         }
         break;
     case WM_RBUTTONDOWN:
