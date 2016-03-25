4cf98f3f11956af32cbdb9cf156cc3d50077c21d
Arthas Lucifer <zimmerk@live.com>
2016-03-25 09:14:13 +0800

    修复CMAKE编译不通过的问题

--- 360SafeDemo/StdAfx.h
+++ 360SafeDemo/StdAfx.h
@@ -15,20 +15,6 @@
 
 using namespace DuiLib;
 
-#ifdef _DEBUG
-#   ifdef _UNICODE
-#       pragma comment(lib, "..\\Lib\\DuiLib_ud.lib")
-#   else
-#       pragma comment(lib, "..\\Lib\\DuiLib_d.lib")
-#   endif
-#else
-#   ifdef _UNICODE
-#       pragma comment(lib, "..\\Lib\\DuiLib_u.lib")
-#   else
-#       pragma comment(lib, "..\\Lib\\DuiLib.lib")
-#   endif
-#endif
-
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
--- GameDemo/StdAfx.h
+++ GameDemo/StdAfx.h
@@ -19,20 +19,6 @@
 
 using namespace DuiLib;
 
-#ifdef _DEBUG
-#   ifdef _UNICODE
-#       pragma comment(lib, "..\\Lib\\DuiLib_ud.lib")
-#   else
-#       pragma comment(lib, "..\\Lib\\DuiLib_d.lib")
-#   endif
-#else
-#   ifdef _UNICODE
-#       pragma comment(lib, "..\\Lib\\DuiLib_u.lib")
-#   else
-#       pragma comment(lib, "..\\Lib\\DuiLib.lib")
-#   endif
-#endif
-
 #include "resource.h"
 
 //{{AFX_INSERT_LOCATION}}
