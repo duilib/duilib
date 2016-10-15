cc338a4806fedf64320b36d70518e26a9dc86294
Atypiape <atypiape@163.com>
2016-10-15 18:41:47 +0800

    将 COptionUI 的 GetSelectBkColor 标记为已弃用，并用 GetSelectedBkColor 替代

--- DuiLib/Control/UIOption.h
+++ DuiLib/Control/UIOption.h
@@ -29,7 +29,8 @@ namespace DuiLib
 		DWORD GetSelectedTextColor();
 
 		void SetSelectedBkColor(DWORD dwBkColor);
-		DWORD GetSelectBkColor();
+		DWORD GetSelectedBkColor();
+		DUI_DEPRECATED DWORD GetSelectBkColor(); // deprecated, use GetSelectedBkColor instead
 
 		LPCTSTR GetForeImage();
 		void SetForeImage(LPCTSTR pStrImage);
