92be8f3571dc41e3182ee6c6c702f15cb2ebdbc3
admins <admin@zoand.com>
2018-05-30 22:03:45 +0800

    头文件声明部分int CPaintManagerUI::MessageLoop()

--- DuiLib/Core/UIManager.h
+++ DuiLib/Core/UIManager.h
@@ -389,7 +389,7 @@ public:
     CControlUI* FindSubControlByClass(CControlUI* pParent, LPCTSTR pstrClass, int iIndex = 0);
     CDuiPtrArray* FindSubControlsByClass(CControlUI* pParent, LPCTSTR pstrClass);
 
-    static void MessageLoop();
+    static int MessageLoop();
     static bool TranslateMessage(const LPMSG pMsg);
 	static void Term();
 
